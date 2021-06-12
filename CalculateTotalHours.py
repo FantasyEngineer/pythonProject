f047f58a6209782023ce0275aa4100cf04186730
DESKTOP-51L5Q8F\34762 <347622814@qq.com>
2021-06-13 02:33:36 +0800

    适配，6月

--- CalculateTotalHours.py
+++ CalculateTotalHours.py
@@ -1,6 +1,7 @@
 import tkinter
 
 from openpyxl import *
+from openpyxl.worksheet.worksheet import Worksheet
 import time  # 引入time模块
 import calendar  # 引入日历
 import chinese_calendar
@@ -13,7 +14,20 @@ from tkinter.messagebox import showinfo
 # 全局化wb
 from pip._internal.utils import subprocess
 
-wb: Workbook = None
+wb: Workbook
+sheet: Worksheet
+
+
+# 点击查看日历
+def lookCal():
+    try:
+        # 获取到日历
+        year = int(yearEntry.get())
+        month = int(MonthEntry.get())
+        cal = calendar.month(year, month)
+        showinfo(message=cal.__str__())
+    except Exception:
+        showinfo(message="年月的输入有错误")
 
 
 # 选择文件
@@ -26,27 +40,62 @@ def fileopen():
             tkinter.messagebox.showwarning(message='表格的路径不能为空！')
             return
         try:
-            # 加载表格
+            # 加载表格,并且赋值道全局
+            global wb
             wb = load_workbook(path)
             var2.set(wb.sheetnames)
             # 将sheet挂上
             frameSheet.pack(padx=10, pady=10, side=TOP, fill=X)
-            # 将运行挂上
-            runBtn.pack(side=BOTTOM, pady=20)
+            # 将查找挂上
+            runBtn1.pack(side=LEFT)
         except Exception:
             tkinter.messagebox.showwarning(message='宝，你好像选择的不是excel文件吧？ 要不再确认一下？')
 
 
+secApartmentIndex: int = 0
+bianzhiIndex: int = 0
+enterTimeIndex: int = 0
+outTimeIndex: int = 0
+
+
+# 查找文字所在的列
+def search():
+    global sheet, secApartmentIndex, bianzhiIndex, enterTimeIndex, outTimeIndex
+    # 获取用户选中的sheet
+    sheetName: str = lb.get(lb.curselection())  # 获取当前选中的文本
+    if sheetName.__eq__(''):
+        tkinter.messagebox.showwarning(message='宝贝，你最少要选择一个sheet，我才能工作啊！')
+
+    isApartMent: bool = 1
+    isbianzhi: bool = 1
+    isenter: bool = 1
+    sheet = wb[sheetName]
+    for i in range(1, sheet.max_row):
+        for y in range(1, sheet.max_column):
+            cell: str = sheet.cell(row=i, column=y).value
+            if cell is None:
+                continue
+            if cell.__eq__("二级部门") and isApartMent:
+                secApartmentIndex = y - 1
+                isApartMent = 0
+            elif cell.__eq__("编制") and isbianzhi:
+                bianzhiIndex = y - 1
+                isbianzhi = 0
+            elif cell.__eq__("入职时间") and isenter:
+                enterTimeIndex = y - 1
+                isenter = 0
+    # 展示数据
+    outTimeIndex = enterTimeIndex + 1
+    indexLabel = Label(window, text=f"二级部门{secApartmentIndex},编制{bianzhiIndex},入职时间{enterTimeIndex},离职时间{outTimeIndex}")
+    indexLabel.pack(side=LEFT)
+
+    # 将运行挂上
+    runBtn.pack(side=BOTTOM, pady=20)
+
+
 def run():
+    print(f"二级部门{secApartmentIndex},编制{bianzhiIndex},入职时间{enterTimeIndex},离职时间{outTimeIndex}")
     try:
-        # 获取用户选中的sheet
-        sheetName: str = lb.get(lb.curselection())  # 获取当前选中的文本
-        if sheetName.__eq__(''):
-            tkinter.messagebox.showwarning(message='宝贝，你最少要选择一个sheet，我才能工作啊！')
-            return
-        path = filePath.get()
-        wb = load_workbook(path)
-        sheet = wb[sheetName]
         print('---------------------时间计算----------------------------')
         # 获取到日历
         year = int(yearEntry.get())
@@ -56,7 +105,7 @@ def run():
         print(f"{year}年{month}月日历:")
         print(cal)
         # 计算当前月有多少天，用以获取最后一天的时间
-        monthLen = calendar._monthlen(year, month)
+        monthLen = calendar.monthlen(year, month)
         # 所以能计算出当前月的第一天，以及最后一天
         monthStartData = f"{year}/{month}/1"
         monthEndData = f"{year}/{month}/{monthLen}"
@@ -74,12 +123,14 @@ def run():
             if isWorkDay:
                 workDayAll.append(day)
 
+        print(workDayAll)
+
         # 每周共有多少工作日
         workDays = len(workDayAll)
         # 每周的每人的工作总小时数，总工作日*8
         totalHourOnePersonMonth = workDays * 8
 
-        print('当月开始时间：' + monthStartData.__str__(), '当月结束时间：' + monthEndData.__str__())
+        print(f'当月开始时间：{monthStartData},当月结束时间：{monthEndData},当月需要工作天数：{workDays}')
 
         # 获取整行数据
         data_all = sheet.rows
@@ -94,15 +145,15 @@ def run():
         data_row = list(data_all)
         for row in data_row:
             # 获取二级部门，下标是1，编制在4，入职时间12，离职时间是13
-            secDepartment: str = row[1].value;
+            secDepartment: str = row[secApartmentIndex].value;
             # 姓名
             name: str = row[2].value
             # 编制
-            bianzhi: str = row[4].value
+            bianzhi: str = row[bianzhiIndex].value
             # 入职时间
-            enterTime: datetime = row[12].value
+            enterTime: datetime = row[enterTimeIndex].value
             # 离职时间
-            outTime: datetime = row[13].value
+            outTime: datetime = row[outTimeIndex].value
 
             # print(secDepartment)
             # 当不是这几个部门的时间，再开始计算这个人的当月的小时数
@@ -115,7 +166,7 @@ def run():
                 if bianzhi.__eq__("正编"):  # 正编
                     # 当是在职状态下的时候，直接将在职改成下个月当天的时间。便于处理
                     if outTime.__str__().strip().__eq__("在职"):
-                        outTime = datetime.now()
+                        outTime = monthEndData
 
                     if outTime >= monthEndData and enterTime < monthStartData:
                         # print(name, "离职时间是大于当月的，或者是月末最后一天，但是全勤")
@@ -126,7 +177,7 @@ def run():
                             print(name, "当月离职的", outTime)
                             for i in range(len(workDayAll)):
                                 if outTime >= workDayAll[i]:
-                                    print(name, outTime, workDayAll[i])
+                                    # print(name, outTime, workDayAll[i])
                                     zhengbianTotalHour += 8
                         # 说明是当月就职的,并且当月不离职
                         elif monthStartData <= enterTime <= monthEndData < outTime:
@@ -145,7 +196,7 @@ def run():
                 else:  # 外包
                     # 当是在职状态下的时候，直接将在职改成下个月当天的时间。便于处理
                     if outTime.__str__().strip().__eq__("在职"):
-                        outTime = datetime.now()
+                        outTime = monthEndData
 
                     if outTime >= monthEndData and enterTime < monthStartData:
                         # print(name, "离职时间是大于当月的，或者是月末最后一天，但是全勤")
@@ -193,6 +244,8 @@ window.geometry('800x400+300+200')
 # ------------------------------创建处理总工时的年月日容器------------------------------
 
 frameTime = Frame(window)
+frameTime.configure(bg='white')
+
 tip = Label(frameTime, text='1.填写需要整理的日期：')
 tip.pack(side=LEFT)
 
@@ -200,28 +253,32 @@ yearEntry = Entry(frameTime, justify=CENTER)
 yearEntry.pack(side=LEFT)
 yearEntry.insert(0, datetime.now().year)
 
-yearlaber = Label(frameTime, text='年')
+yearlaber = Label(frameTime, text='年', bg='white')
 yearlaber.pack(side=LEFT)
 
 MonthEntry = Entry(frameTime, justify=CENTER)
 MonthEntry.pack(side=LEFT)
 MonthEntry.insert(0, datetime.now().month - 1)
 
-monthlaber = Label(frameTime, text='月')
+monthlaber = Label(frameTime, text='月', bg='white')
 monthlaber.pack(side=LEFT)
+
+lookCalBtn = Button(frameTime, text="查看日历", width='30', command=lookCal)
+lookCalBtn.pack(side=LEFT)
 
 frameTime.pack(padx=10, pady=10, fill=X)
 
 # ------------------------------创建选择文件的容器------------------------------
 frameSelectFile = Frame(window)
 frameSelectFile.pack(padx=10, pady=10, fill=X)
+frameSelectFile.configure(bg='white')
 # 创建button
-btn = Button(frameSelectFile, text='2.点击选择操作的文件', font=("宋体", 14), command=fileopen)
+btn = Button(frameSelectFile, text='2.点击选择操作的文件', command=fileopen)
 btn.pack(side=LEFT)
 # 创建展示文件夹路径的输入框
 filePath = StringVar()
 filePathEntry = Entry(frameSelectFile, width=100, textvariable=filePath)
-filePathEntry.pack(side=RIGHT, fill=X)
+filePathEntry.pack(padx=8, side=RIGHT, fill=X)
 
 # ------------------------------创建展示sheets的listbox------------------------------
 frameSheet = Frame(window)
@@ -234,6 +291,7 @@ lb.pack(side=TOP, fill=X)
 
 # ------------------------------创建运行按钮------------------------------
 runBtn = Button(window, text="运行", width='30', bg='red', command=run)
+runBtn1 = Button(window, text="查找所在字符所在的列数", width='30', command=search)
 
 # 窗体循环
 window.mainloop()
